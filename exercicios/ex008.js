2d8c7f08a36b4b1eb6322f1152f6fe918ba62006
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-10 21:41:05 -0300

    Create ex008.js

---
+++ exercicios/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Calma Toretto, tome multa!')
+} else {
+    console.log('Vá em paz e siga em frente')
+}
+console.log('Dirija sempre usando cinto de segurança!')
