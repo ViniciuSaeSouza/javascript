498d12d5000146d526cac3301aa6bbaa2b49e8db
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-14 17:58:21 -0300

    Create teste.js

---
+++ exercicios/aula13/teste.js
@@ -0,0 +1,13 @@
+var counter = 1
+
+
+ while (counter <= 6) { //Repetição de teste lógico no início
+    console.log(`Passo ${counter}`)
+    counter++ // counter++ == counter = counter + 1
+} 
+
+
+do {
+    console.log(`Passo ${counter}`)
+    counter++
+} while(counter <= 8) //Repetição de teste lógico no final
