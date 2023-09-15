bb0bdb293b9875ac66e3ec36a432e96a5b2ccc6b
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-15 11:53:39 -0300

    Update ambiente.js

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,3 +1,10 @@
-let num = [8 , 24 , 2]
+let num = [3 , 1 ,2]  
+num[3] = 4
+num.push(5)
+num.sort()
+console.log(num)
+console.log(`O Número de elementos é: ${num.length}`)
 
-console.log(num )
+for (let pos=0;pos<num.length;pos++) {
+    console.log(`Este é o elemento ${pos} e tem o valor: ${num[pos]}`)
+}
