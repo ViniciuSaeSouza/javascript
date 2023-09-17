26bcc5fc56fbb9a8b4c1685ea654eb05ab9ef542
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-17 18:04:07 -0300

    finalized desafio03

--- desafios/desafio03/script.js
+++ desafios/desafio03/script.js
@@ -1,26 +1,29 @@
-    
 function contar() {
-    
+  let start = document.querySelector("input#start");
+  let end = document.querySelector("input#end");
+  let step = document.querySelector("input#step");
+  let res = document.querySelector("div#res");
   if (
     start.value.length == 0 ||
     end.value.length == 0 ||
     step.value.length == 0
-  ) {
-    window.alert = "Verifique os dados e tente novamente!";
-  } else {
-    let start = document.querySelector("input#start");
-    let end = document.querySelector("input#end");
-    let step = document.querySelector("input#step");
-    let res = document.querySelector("div#res");
+  ){
+    window.alert ("Verifique os dados e tente novamente!");
+  }
+   else {
     res.innerHTML = "Contando: ";
-    if (start < end) {
+    if (step.value <= 0) {
+        res.innerHTML = 'Passo vazio! Considerando passo = 1 <br>'
+        step.value = 1
+    }
+    if (start.value < end.value) {
       for (
         c = Number(start.value);
         c <= Number(end.value);
         c += Number(step.value)
       ) {
-        res.innerHTML += `${c}  `;
-        console.log(c)
+        res.innerHTML += `${c} // `;
+        console.log(c);
       }
     } else {
       for (
@@ -28,12 +31,10 @@ function contar() {
         c >= Number(end.value);
         c -= Number(step.value)
       ) {
-        res.innerHTML += `${c}  `;
-        console.log(c)
+        res.innerHTML += `${c} // `;
+        console.log(c);
       }
     }
+    res.innerHTML += '\🏁'
   }
 }
-
-console.log(contar())
-
