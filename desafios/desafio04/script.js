a1f847d94b321095b2b6b5a5585d45402cbae115
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-18 17:32:33 -0300

    final commit

--- desafios/desafio04/script.js
+++ desafios/desafio04/script.js
@@ -1,13 +1,15 @@
 function calc() {
   let num = document.querySelector("input#num");
-  let disp = document.querySelector("div#disp");
+  let disp = document.querySelector("select#selTab");
   disp.innerHTML = "";
   if (num.value.length == "0") {
     window.alert("Favor digitar um número!");
   } else {
     for (c = 1; c <= 10; c++) {
       let res = num.value * c;
-      disp.innerHTML += `<br> ${num.value} X ${c} = ${res} <br>`;
+      item = document.createElement('option')
+      item.text += ` ${num.value} X ${c} = ${res} `;
+      disp.appendChild(item)
     }
   }
 }
