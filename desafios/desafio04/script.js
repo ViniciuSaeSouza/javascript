867343aa118ac6f97900dd5e3c48b293e74862be
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-18 17:16:22 -0300

    JS logic initial commit

--- desafios/desafio04/script.js
+++ desafios/desafio04/script.js
@@ -1,9 +1,13 @@
-function calc(){
-    let num = document.querySelector('input#num')
-    let disp = document.querySelector('div#disp')
-    for (c = 1; c <= 10; c++ ) {
-        let res = num.value * c
-        disp.innerHTML = `${num.value} X ${c} = ${res}`
+function calc() {
+  let num = document.querySelector("input#num");
+  let disp = document.querySelector("div#disp");
+  disp.innerHTML = "";
+  if (num.value.length == "0") {
+    window.alert("Favor digitar um número!");
+  } else {
+    for (c = 1; c <= 10; c++) {
+      let res = num.value * c;
+      disp.innerHTML += `<br> ${num.value} X ${c} = ${res} <br>`;
     }
-
+  }
 }
