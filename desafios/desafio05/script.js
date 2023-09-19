5d38d9f51b02ee744e9afcb7cdea702f6d75299e
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-19 18:26:54 -0300

    Update script.js

--- desafios/desafio05/script.js
+++ desafios/desafio05/script.js
@@ -3,12 +3,14 @@ let board = document.querySelector("select#board");
 let info = document.querySelector("div#info");
 let values = [];
 
+//Escuta para quando apertar 'Enter' acionar o botão "Adicionar"
 number.addEventListener("keydown", function (enter) {
   if (enter.key === "Enter") {
     document.getElementById("addBtn").click();
   }
 });
 
+//Verifica se o número(n) está entre 1 e 100
 function numberCheck(n) {
   if (Number(n) >= 1 && Number(n) <= 100) {
     return true;
@@ -17,6 +19,7 @@ function numberCheck(n) {
   }
 }
 
+//Verifica se o número(n) já está dentro do array values(v)
 function boardCheck(n, v) {
   if (v.indexOf(Number(n)) != -1) {
     return true;
@@ -25,6 +28,7 @@ function boardCheck(n, v) {
   }
 }
 
+//Adiciona o número ao elemento 'board' e ao array 'values' se passar nos testes numberCheck() e boardCheck()
 function addToBoard() {
   if (numberCheck(number.value) && !boardCheck(number.value, values)) {
     values.push(Number(number.value));
