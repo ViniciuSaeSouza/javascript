76a256e95faf3db8322f62d5dcd391660ad16199
Vinicius Saes <viniciusaesouza@gmail.com>
2023-09-19 11:05:16 -0300

    refactor addBoard() + added eventlistener

--- desafios/desafio05/script.js
+++ desafios/desafio05/script.js
@@ -3,32 +3,38 @@ let board = document.querySelector("select#board");
 let info = document.querySelector("div#info");
 let values = [];
 
+number.addEventListener("keydown", function (enter) {
+  if (enter.key === "Enter") {
+    document.querySelector("addBtn").click();
+  }
+});
+
 function numberCheck(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function boardCheck(n, v){
-    if (v.indexOf(Number(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
+function boardCheck(n, v) {
+  if (v.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 
 function addToBoard() {
-    if (numberCheck(number.value) && boardCheck(number.value, values)) {
-        window.alert('teste')
-    } else {
-        window.alert('Número fora do limite ou já presente na lista!')
-    }
+  if (numberCheck(number.value) && !boardCheck(number.value, values)) {
+    boardValue = document.createElement("option");
+    boardValue.text += `${number.value} adicionado`;
+    board.appendChild(boardValue);
+    values.push(number.value);
+  } else {
+    window.alert("Número fora do limite ou já presente na lista!");
+  }
 }
-
-
 
 /*function addToBoard() {
     if (values.indexOf(number.value) == -1 && number.value > 1 || number.value < 100) {
@@ -42,4 +48,4 @@ function addToBoard() {
       
     }
   }
-*/  
+*/
